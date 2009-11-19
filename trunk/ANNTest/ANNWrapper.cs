8472891d8e6767beb8b396fe35fdfe40ae1833d2
damoguyan8844 <damoguyan8844@2e4fddd8-cc6a-11de-b393-33af5e5426e5>
2009-11-19 11:22:02 +0000

    Commit Change

--- trunk/ANNTest/ANNWrapper.cs
+++ trunk/ANNTest/ANNWrapper.cs
@@ -22,8 +22,41 @@ namespace ANNTest
         public static extern bool SaveBlockToBMP(string bmpFile, 
                 double leftRate, double topRate, double rightRate, double bottomRate,
                 string blockBMPFile);
+        
+        [DllImport("ANNRecognition.dll", EntryPoint = "SaveBlockToBMP2")]
+        public static extern bool SaveBlockToBMP2(string bmpFile,
+                long leftRate, long topRate, long rightRate, long bottomRate,
+                string blockBMPFile);
 
         [DllImport("ANNRecognition.dll", EntryPoint = "OCRFile")]
-        public static extern bool OCRFile(string fullFileName,out string strContent);
+        public static extern bool OCRFile(string bmpFile,out string content);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "ReadDIBFile")]
+        public static extern IntPtr ReadDIBFile( string fileName);
+
+        //
+        [DllImport("ANNRecognition.dll", EntryPoint = "BPEncode")]
+        public static extern bool BPEncode(IntPtr hInputDIB, double[] outCode, long top, long left, long right, long bottom);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "LoadBPParameters")]
+        public static extern bool LoadBPParameters(string paraFile);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "SaveBPParameters")]
+        public static extern bool SaveBPParameters(string paraFile);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "PrintBPParameters")]
+        public static extern bool PrintBPParameters(string txtParaFile);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "InitTrainBPLearnSpeed")]
+        public static extern bool InitTrainBPLearnSpeed(double learningSpeed);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "InitBPParameters")]
+        public static extern bool InitBPParameters(int inputDim,int implicitDim,int outputDim,double[][] w1,double[] b1,double[][] w2,double[] b2);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "Training")]
+        public static extern double Training(double[] input, double[] dest);
+
+        [DllImport("ANNRecognition.dll", EntryPoint = "ReleaseDIBFile")]
+        public static extern bool ReleaseDIBFile(IntPtr hInputDIB);
     }
 }
