18b4612422e6432ef79028eafb9d2b332a1290b7
damoguyan8844 <damoguyan8844@2e4fddd8-cc6a-11de-b393-33af5e5426e5>
2009-11-19 11:22:02 +0000

    Commit Change

--- ANNTest/forMain.Designer.cs
+++ ANNTest/forMain.Designer.cs
@@ -46,11 +46,31 @@ namespace ANNTest
             this.button6 = new System.Windows.Forms.Button();
             this.textOCRContent = new System.Windows.Forms.TextBox();
             this.labelPath = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textTraingInputs = new System.Windows.Forms.TextBox();
+            this.textBMPFolders = new System.Windows.Forms.TextBox();
+            this.textSubsystemBMP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btTraining = new System.Windows.Forms.Button();
+            this.textToPath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textSpeed = new System.Windows.Forms.TextBox();
+            this.textAvrgDiff = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chkAutoSave = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textParas = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textParaFactor = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(359, 36);
+            this.button1.Location = new System.Drawing.Point(337, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 28);
             this.button1.TabIndex = 0;
@@ -84,7 +104,7 @@ namespace ANNTest
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(359, 66);
+            this.button2.Location = new System.Drawing.Point(460, 13);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(117, 27);
             this.button2.TabIndex = 4;
@@ -94,7 +114,7 @@ namespace ANNTest
             // 
             // textOutPutBMP
             // 
-            this.textOutPutBMP.Location = new System.Drawing.Point(198, 43);
+            this.textOutPutBMP.Location = new System.Drawing.Point(204, 13);
             this.textOutPutBMP.Name = "textOutPutBMP";
             this.textOutPutBMP.Size = new System.Drawing.Size(100, 21);
             this.textOutPutBMP.TabIndex = 5;
@@ -102,7 +122,7 @@ namespace ANNTest
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(359, 99);
+            this.button3.Location = new System.Drawing.Point(583, 13);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(114, 30);
             this.button3.TabIndex = 6;
@@ -112,7 +132,7 @@ namespace ANNTest
             // 
             // textInputInt
             // 
-            this.textInputInt.Location = new System.Drawing.Point(192, 108);
+            this.textInputInt.Location = new System.Drawing.Point(204, 45);
             this.textInputInt.Name = "textInputInt";
             this.textInputInt.Size = new System.Drawing.Size(106, 21);
             this.textInputInt.TabIndex = 7;
@@ -120,7 +140,7 @@ namespace ANNTest
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(359, 135);
+            this.button4.Location = new System.Drawing.Point(337, 46);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(113, 28);
             this.button4.TabIndex = 8;
@@ -130,17 +150,17 @@ namespace ANNTest
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(359, 169);
+            this.button5.Location = new System.Drawing.Point(716, 113);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(111, 34);
+            this.button5.Size = new System.Drawing.Size(134, 21);
             this.button5.TabIndex = 9;
-            this.button5.Text = "SaveBlockToBMP";
+            this.button5.Text = "(Rate)SaveBlockToBMP";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // textLeft
             // 
-            this.textLeft.Location = new System.Drawing.Point(12, 177);
+            this.textLeft.Location = new System.Drawing.Point(204, 125);
             this.textLeft.Name = "textLeft";
             this.textLeft.Size = new System.Drawing.Size(62, 21);
             this.textLeft.TabIndex = 10;
@@ -148,7 +168,7 @@ namespace ANNTest
             // 
             // textTop
             // 
-            this.textTop.Location = new System.Drawing.Point(94, 177);
+            this.textTop.Location = new System.Drawing.Point(276, 125);
             this.textTop.Name = "textTop";
             this.textTop.Size = new System.Drawing.Size(67, 21);
             this.textTop.TabIndex = 11;
@@ -156,7 +176,7 @@ namespace ANNTest
             // 
             // textRight
             // 
-            this.textRight.Location = new System.Drawing.Point(176, 177);
+            this.textRight.Location = new System.Drawing.Point(354, 125);
             this.textRight.Name = "textRight";
             this.textRight.Size = new System.Drawing.Size(72, 21);
             this.textRight.TabIndex = 12;
@@ -164,7 +184,7 @@ namespace ANNTest
             // 
             // textBottom
             // 
-            this.textBottom.Location = new System.Drawing.Point(265, 177);
+            this.textBottom.Location = new System.Drawing.Point(432, 125);
             this.textBottom.Name = "textBottom";
             this.textBottom.Size = new System.Drawing.Size(65, 21);
             this.textBottom.TabIndex = 13;
@@ -173,7 +193,7 @@ namespace ANNTest
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 156);
+            this.label1.Location = new System.Drawing.Point(202, 110);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(131, 12);
             this.label1.TabIndex = 14;
@@ -181,9 +201,9 @@ namespace ANNTest
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(359, 225);
+            this.button6.Location = new System.Drawing.Point(583, 45);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(110, 29);
+            this.button6.Size = new System.Drawing.Size(114, 29);
             this.button6.TabIndex = 15;
             this.button6.Text = "OCRFile";
             this.button6.UseVisualStyleBackColor = true;
@@ -191,7 +211,7 @@ namespace ANNTest
             // 
             // textOCRContent
             // 
-            this.textOCRContent.Location = new System.Drawing.Point(12, 247);
+            this.textOCRContent.Location = new System.Drawing.Point(466, 76);
             this.textOCRContent.Name = "textOCRContent";
             this.textOCRContent.Size = new System.Drawing.Size(317, 21);
             this.textOCRContent.TabIndex = 16;
@@ -199,17 +219,215 @@ namespace ANNTest
             // labelPath
             // 
             this.labelPath.AutoSize = true;
-            this.labelPath.Location = new System.Drawing.Point(11, 226);
+            this.labelPath.Location = new System.Drawing.Point(335, 79);
             this.labelPath.Name = "labelPath";
             this.labelPath.Size = new System.Drawing.Size(119, 12);
             this.labelPath.TabIndex = 17;
             this.labelPath.Text = "OCRFile Full  Path ";
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(221, 171);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "BPEncode";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textTraingInputs
+            // 
+            this.textTraingInputs.Location = new System.Drawing.Point(28, 200);
+            this.textTraingInputs.MaxLength = 3276700;
+            this.textTraingInputs.Multiline = true;
+            this.textTraingInputs.Name = "textTraingInputs";
+            this.textTraingInputs.Size = new System.Drawing.Size(822, 315);
+            this.textTraingInputs.TabIndex = 19;
+            // 
+            // textBMPFolders
+            // 
+            this.textBMPFolders.Location = new System.Drawing.Point(28, 171);
+            this.textBMPFolders.Name = "textBMPFolders";
+            this.textBMPFolders.Size = new System.Drawing.Size(162, 21);
+            this.textBMPFolders.TabIndex = 20;
+            this.textBMPFolders.Text = "BMPFolders";
+            // 
+            // textSubsystemBMP
+            // 
+            this.textSubsystemBMP.Location = new System.Drawing.Point(29, 126);
+            this.textSubsystemBMP.Name = "textSubsystemBMP";
+            this.textSubsystemBMP.Size = new System.Drawing.Size(161, 21);
+            this.textSubsystemBMP.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "SubSystem BMP ";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(716, 140);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(134, 22);
+            this.button8.TabIndex = 23;
+            this.button8.Text = "(Long)SaveBlocToBMP";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btTraining
+            // 
+            this.btTraining.Location = new System.Drawing.Point(636, 535);
+            this.btTraining.Name = "btTraining";
+            this.btTraining.Size = new System.Drawing.Size(109, 25);
+            this.btTraining.TabIndex = 24;
+            this.btTraining.Text = "Training";
+            this.btTraining.UseVisualStyleBackColor = true;
+            this.btTraining.Click += new System.EventHandler(this.btTraining_Click);
+            // 
+            // textToPath
+            // 
+            this.textToPath.Location = new System.Drawing.Point(514, 124);
+            this.textToPath.Name = "textToPath";
+            this.textToPath.Size = new System.Drawing.Size(124, 21);
+            this.textToPath.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(513, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "To Path";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 12);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "BMP Folders";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 521);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 12);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Learning Speed";
+            // 
+            // textSpeed
+            // 
+            this.textSpeed.Location = new System.Drawing.Point(29, 538);
+            this.textSpeed.Name = "textSpeed";
+            this.textSpeed.Size = new System.Drawing.Size(100, 21);
+            this.textSpeed.TabIndex = 29;
+            this.textSpeed.Text = "0.05";
+            // 
+            // textAvrgDiff
+            // 
+            this.textAvrgDiff.Location = new System.Drawing.Point(163, 538);
+            this.textAvrgDiff.Name = "textAvrgDiff";
+            this.textAvrgDiff.Size = new System.Drawing.Size(117, 21);
+            this.textAvrgDiff.TabIndex = 30;
+            this.textAvrgDiff.Text = "0.0015";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(161, 523);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 12);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Accept Average Diff";
+            // 
+            // chkAutoSave
+            // 
+            this.chkAutoSave.AutoSize = true;
+            this.chkAutoSave.Checked = true;
+            this.chkAutoSave.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAutoSave.Location = new System.Drawing.Point(495, 540);
+            this.chkAutoSave.Name = "chkAutoSave";
+            this.chkAutoSave.Size = new System.Drawing.Size(102, 16);
+            this.chkAutoSave.TabIndex = 32;
+            this.chkAutoSave.Text = "AutoSaveParas";
+            this.chkAutoSave.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(312, 525);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 12);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "BP Paras File";
+            // 
+            // textParas
+            // 
+            this.textParas.Location = new System.Drawing.Point(308, 538);
+            this.textParas.Name = "textParas";
+            this.textParas.Size = new System.Drawing.Size(142, 21);
+            this.textParas.TabIndex = 34;
+            this.textParas.Text = "001278.dat";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(762, 536);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(87, 23);
+            this.btnStop.TabIndex = 35;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textParaFactor
+            // 
+            this.textParaFactor.Location = new System.Drawing.Point(456, 173);
+            this.textParaFactor.Name = "textParaFactor";
+            this.textParaFactor.Size = new System.Drawing.Size(141, 21);
+            this.textParaFactor.TabIndex = 36;
+            this.textParaFactor.Text = "16";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(371, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 12);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Divide Factor";
+            // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 393);
+            this.ClientSize = new System.Drawing.Size(862, 580);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textParaFactor);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.textParas);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.chkAutoSave);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textAvrgDiff);
+            this.Controls.Add(this.textSpeed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textToPath);
+            this.Controls.Add(this.btTraining);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textSubsystemBMP);
+            this.Controls.Add(this.textBMPFolders);
+            this.Controls.Add(this.textTraingInputs);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.labelPath);
             this.Controls.Add(this.textOCRContent);
             this.Controls.Add(this.button6);
@@ -255,6 +473,26 @@ namespace ANNTest
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.TextBox textOCRContent;
         private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textTraingInputs;
+        private System.Windows.Forms.TextBox textBMPFolders;
+        private System.Windows.Forms.TextBox textSubsystemBMP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btTraining;
+        private System.Windows.Forms.TextBox textToPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textSpeed;
+        private System.Windows.Forms.TextBox textAvrgDiff;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chkAutoSave;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textParas;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox textParaFactor;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- ANNTest/forMain.cs
+++ ANNTest/forMain.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ANNTest
 {
@@ -37,7 +39,7 @@ namespace ANNTest
 
         private void button5_Click(object sender, EventArgs e)
         {
-            ANNWrapper.SaveBlockToBMP(textInputBMP.Text,
+            ANNWrapper.SaveBlockToBMP(textSubsystemBMP.Text,
                 Double.Parse(textLeft.Text),
                 Double.Parse(textTop.Text),
                 Double.Parse(textRight.Text),
@@ -51,6 +53,170 @@ namespace ANNTest
             labelPath.Text = Application.StartupPath + "\\" + textInputTIF.Text;
             ANNWrapper.OCRFile(Application.StartupPath +"\\"+ textInputTIF.Text, out strContent);
             textOCRContent.Text = strContent;
+        }
+
+        private void EncodeBMPs(DirectoryInfo Dir, int dest)
+        {
+            double[] dblEncode = new double[64];
+
+            try
+            {
+                foreach (FileInfo f in Dir.GetFiles("*.bmp"))
+                {
+                    IntPtr hdibHandle = ANNWrapper.ReadDIBFile(f.FullName);
+                    if (ANNWrapper.BPEncode(hdibHandle, dblEncode, 0, 0, 0, 0))
+                    {
+                        string strCodes = "";
+                        foreach (double dblValue in dblEncode)
+                        {
+                            strCodes += dblValue.ToString() + ",";
+                        }
+
+                        strCodes += dest.ToString();
+                        if (textTraingInputs.Lines.Length == 0)
+                            textTraingInputs.AppendText(strCodes);
+                        else
+                            textTraingInputs.AppendText("\r\n" + strCodes);
+                    }
+                    ANNWrapper.ReleaseDIBFile(hdibHandle);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }   
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            if (textParas.Text.Length > 0)
+                ANNWrapper.LoadBPParameters(Application.StartupPath + "\\" + textParas.Text);
+            else
+                ANNWrapper.InitBPParameters(64, 8, 4, null, null, null, null);
+
+
+            DirectoryInfo Dir = new DirectoryInfo(Application.StartupPath + "\\"+textBMPFolders.Text);
+            try
+            {
+                foreach (DirectoryInfo d in Dir.GetDirectories())       
+                {
+                    if(d.ToString()=="BMP0")
+                        EncodeBMPs(d,0);
+                    else if(d.ToString()=="BMP1")
+                        EncodeBMPs(d,1);
+                    else if(d.ToString()=="BMP2")
+                        EncodeBMPs(d,2);
+                    else if(d.ToString()=="BMP3")
+                        EncodeBMPs(d,3);
+                    else if(d.ToString()=="BMP4")
+                        EncodeBMPs(d,4);
+                    else if(d.ToString()=="BMP5")
+                        EncodeBMPs(d,5);
+                    else if(d.ToString()=="BMP6")
+                        EncodeBMPs(d,6);
+                    else if (d.ToString() == "BMP7")
+                        EncodeBMPs(d,7);
+                    else if(d.ToString()=="BMP8")
+                        EncodeBMPs(d,8);
+                    else if(d.ToString()== "BMP9")
+                        EncodeBMPs(d,9);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }   
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ANNWrapper.SaveBlockToBMP2(textSubsystemBMP.Text,
+             Int32.Parse(textLeft.Text),
+             Int32.Parse(textTop.Text),
+             Int32.Parse(textRight.Text),
+             Int32.Parse(textBottom.Text),
+             textOutPutBMP.Text);
+        }
+
+        private bool m_stop = false;
+        private void btTraining_Click(object sender, EventArgs e)
+        {
+            m_stop = false;
+            btTraining.Enabled = false;
+
+            if (textParas.Text.Length > 0)
+                ANNWrapper.LoadBPParameters(Application.StartupPath + "\\" + textParas.Text);
+            else
+                ANNWrapper.InitBPParameters(64,8,4,null,null,null,null);
+
+            if (textTraingInputs.Lines.Length<1)
+                return ;
+            
+            int divideFactor = Int32.Parse(textParaFactor.Text);
+
+            ANNWrapper.InitTrainBPLearnSpeed(Double.Parse(textSpeed.Text));
+            double accpt_diff = Double.Parse(textAvrgDiff.Text.ToString());
+
+            double[] inputs = new double[64];
+            double[] dests = new double[4];
+            while(true)
+            {
+                double this_dif=0.0;
+                foreach(string line in textTraingInputs.Lines)
+                {
+                    string[] strs = line.Split(',');
+                    if(strs.Length!=65) continue; 
+                    
+                    for(int i=0;i<64;i++)
+                    {
+                        inputs[i] = Double.Parse(strs[i]) / divideFactor;
+                    }
+
+                    string dest=Convert.ToString(Int32.Parse(strs[64]), 2);
+
+                    if (dest.Length > 0 && dest[0] == '1')
+                        dests[0] = 1.0;
+                    else
+                        dests[0] = 0.0;
+
+                    if (dest.Length > 1 && dest[1] == '1')
+                        dests[1] = 1.0;
+                    else
+                        dests[1] = 0.0;
+
+                    if (dest.Length > 2 && dest[2] == '1')
+                        dests[2] = 1.0;
+                    else
+                        dests[2] = 0.0;
+
+                    if (dest.Length > 3 && dest[3] == '1')
+                        dests[3] = 1.0;
+                    else
+                        dests[3] = 0.0;
+
+                    double dif=ANNWrapper.Training(inputs, dests);
+                    this_dif += dif;
+                }
+
+                this_dif /= textTraingInputs.Lines.Length;
+
+                if (chkAutoSave.Checked)
+                    ANNWrapper.SaveBPParameters(Application.StartupPath + "\\" + textParas.Text);
+
+                btTraining.Text=this_dif.ToString();
+                if(this_dif<=accpt_diff || m_stop==true)
+                {
+                    ANNWrapper.SaveBPParameters(Application.StartupPath + "\\" + textParas.Text);
+                    break;
+                }
+            }
+            btTraining.Enabled =true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            m_stop = true;
         }
     }
 }
