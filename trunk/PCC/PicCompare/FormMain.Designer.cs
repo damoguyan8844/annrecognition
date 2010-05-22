1edfe77c82bd013ca584fbea41800a17f8362404
damoguyan8844 <damoguyan8844@2e4fddd8-cc6a-11de-b393-33af5e5426e5>
2010-05-22 11:46:29 +0000

    Add Files

---
+++ trunk/PCC/PicCompare/FormMain.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace PicCompare
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu = new System.Windows.Forms.MainMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.chkR = new System.Windows.Forms.CheckBox();
+            this.chkG = new System.Windows.Forms.CheckBox();
+            this.chkB = new System.Windows.Forms.CheckBox();
+            this.chkGray = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.MenuItems.Add(this.menuItem1);
+            this.mainMenu.MenuItems.Add(this.menuItem4);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.MenuItems.Add(this.menuItem2);
+            this.menuItem1.MenuItems.Add(this.menuItem3);
+            this.menuItem1.Text = "菜单";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "选择图片1";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Text = "选择图片2";
+            this.menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Text = "对比";
+            this.menuItem4.Click += new System.EventHandler(this.menuItem4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(35, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.Text = "结果：";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(85, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.Text = "0.0%";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.None;
+            this.tabControl1.Location = new System.Drawing.Point(0, 39);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(240, 200);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(0, 0);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(240, 177);
+            this.tabPage1.Text = "图片1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 177);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Location = new System.Drawing.Point(0, 0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(232, 174);
+            this.tabPage2.Text = "图片2";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(232, 174);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            // 
+            // chkR
+            // 
+            this.chkR.Location = new System.Drawing.Point(3, 245);
+            this.chkR.Name = "chkR";
+            this.chkR.Size = new System.Drawing.Size(42, 20);
+            this.chkR.TabIndex = 7;
+            this.chkR.Text = "R";
+            this.chkR.CheckStateChanged += new System.EventHandler(this.chkR_CheckStateChanged);
+            // 
+            // chkG
+            // 
+            this.chkG.Location = new System.Drawing.Point(51, 245);
+            this.chkG.Name = "chkG";
+            this.chkG.Size = new System.Drawing.Size(42, 17);
+            this.chkG.TabIndex = 8;
+            this.chkG.Text = "G";
+            this.chkG.CheckStateChanged += new System.EventHandler(this.chkG_CheckStateChanged);
+            // 
+            // chkB
+            // 
+            this.chkB.Location = new System.Drawing.Point(100, 246);
+            this.chkB.Name = "chkB";
+            this.chkB.Size = new System.Drawing.Size(34, 15);
+            this.chkB.TabIndex = 9;
+            this.chkB.Text = "B";
+            this.chkB.CheckStateChanged += new System.EventHandler(this.chkB_CheckStateChanged);
+            // 
+            // chkGray
+            // 
+            this.chkGray.Checked = true;
+            this.chkGray.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkGray.Location = new System.Drawing.Point(153, 244);
+            this.chkGray.Name = "chkGray";
+            this.chkGray.Size = new System.Drawing.Size(57, 18);
+            this.chkGray.TabIndex = 10;
+            this.chkGray.Text = "Gray";
+            this.chkGray.CheckStateChanged += new System.EventHandler(this.chkGray_CheckStateChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.chkGray);
+            this.Controls.Add(this.chkB);
+            this.Controls.Add(this.chkG);
+            this.Controls.Add(this.chkR);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Menu = this.mainMenu;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "图像匹配度计算：";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox chkR;
+        private System.Windows.Forms.CheckBox chkG;
+        private System.Windows.Forms.CheckBox chkB;
+        private System.Windows.Forms.CheckBox chkGray;
+    }
+}
+
